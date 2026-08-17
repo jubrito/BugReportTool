3c4268382cb18172137fe519f663aa2d47a1afc3
jubrito <jujubrito@outlook.com>
2026-08-17 18:51:44 -0300

    style: revert changes

--- src/slides/Slide3Solution.jsx
+++ src/slides/Slide3Solution.jsx
@@ -82,8 +82,11 @@ export default function Slide3Solution() {
               </p>
               <p>
                 → No friction for quick reports, but the right prompts are
-                impossible to miss. Users know what info is most useful to
-                provide if they want a faster resolution.
+                impossible to miss.
+              </p>
+              <p>
+                → Users know what info is most useful to provide if they want a
+                faster resolution.
               </p>
             </aside>
           </Reveal>
